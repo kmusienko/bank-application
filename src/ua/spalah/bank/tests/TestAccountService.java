92299b3bdde2cf8b438d3fa7bb066ed9268a3d41
Kostya Musienko <kmusienko97@gmail.com>
2017-01-26 16:46:32 +0200

    Unit tests for AccountService, ClientService

---
+++ src/ua/spalah/bank/tests/TestAccountService.java
@@ -0,0 +1,64 @@
+package ua.spalah.bank.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ua.spalah.bank.exceptions.NotEnoughFundsException;
+import ua.spalah.bank.models.accounts.Account;
+import ua.spalah.bank.models.accounts.CheckingAccount;
+import ua.spalah.bank.models.accounts.SavingAccount;
+import ua.spalah.bank.services.AccountService;
+import ua.spalah.bank.services.impl.AccountServiceImpl;
+
+/**
+ * Created by Kostya on 26.01.2017.
+ */
+public class TestAccountService extends Assert{
+   private AccountService accountService;
+   private Account saving;
+   private Account checking;
+
+    @Before
+    public void init() {
+        accountService = new AccountServiceImpl();
+        saving = new SavingAccount(500);
+        checking = new CheckingAccount(500, 200);
+    }
+    @Test
+    public void testDeposit() {
+        accountService.deposit(saving, 110);
+        assertEquals(610, saving.getBalance(),0);
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testDepositNegativeAmount() {
+        accountService.deposit(saving, -20);
+    }
+    @Test
+    public void testWithdrawSaving() throws NotEnoughFundsException {
+        accountService.withdraw(saving, 450);
+        assertEquals(50, saving.getBalance(),0);
+    }
+    @Test
+    public void testWithdrawChecking() throws NotEnoughFundsException {
+        accountService.withdraw(checking, 600);
+        assertEquals(-100, checking.getBalance(), 0);
+    }
+    @Test (expected = NotEnoughFundsException.class)
+    public void testWithdrawSavingNotFunds() throws NotEnoughFundsException {
+        accountService.withdraw(saving, 501);
+    }
+    @Test (expected = NotEnoughFundsException.class)
+    public void testWithdrawCheckingNotFunds() throws NotEnoughFundsException {
+        accountService.withdraw(checking, 701);
+    }
+    @Test
+    public void testTransfer() throws NotEnoughFundsException {
+        double amount = 400;
+        double expSavingBalance = saving.getBalance() - amount;
+        double expCheckingBalance = checking.getBalance() + amount;
+        accountService.transfer(saving, checking, amount);
+        assertEquals(expSavingBalance, saving.getBalance(), 0);
+        assertEquals(expCheckingBalance, checking.getBalance(), 0);
+    }
+
+}
---
+++ src/ua/spalah/bank/tests/TestClientService.java
@@ -0,0 +1,125 @@
+package ua.spalah.bank.tests;
+
+import org.junit.*;
+import ua.spalah.bank.exceptions.ClientAlreadyExistsException;
+import ua.spalah.bank.exceptions.ClientNotFoundException;
+import ua.spalah.bank.models.Bank;
+import ua.spalah.bank.models.Client;
+import ua.spalah.bank.models.accounts.Account;
+import ua.spalah.bank.models.accounts.CheckingAccount;
+import ua.spalah.bank.models.accounts.SavingAccount;
+import ua.spalah.bank.models.type.Gender;
+import ua.spalah.bank.services.ClientService;
+import ua.spalah.bank.services.impl.ClientServiceImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Kostya on 26.01.2017.
+ */
+public class TestClientService extends Assert{
+    private Bank bank;
+    private ClientService clientService;
+
+    @Before
+    public void init() {
+        bank = new Bank();
+        clientService = new ClientServiceImpl();
+        bank.getAllClients().put("Kostya", new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro"));
+        bank.getAllClients().put("Misha", new Client("Misha", Gender.MALE, "mixa@ukr.net", "+380674567890", "Odessa"));
+        bank.getAllClients().put("Gera",  new Client("Gera", Gender.FEMALE, "gerahello@gmail.com", "+380964561234", "Dnipro"));
+//        CheckingAccount c1 = new CheckingAccount(1000, 800);
+//        SavingAccount s1 = new SavingAccount(3000);
+//        CheckingAccount c2 = new CheckingAccount(17000, 7000);
+//        SavingAccount s2 = new SavingAccount(5000);
+//        CheckingAccount c3 = new CheckingAccount(2000, 1000);
+//        SavingAccount s3 = new SavingAccount(20000);
+//        bank.getAllClients().get("Kostya").getAccounts().add(c1);
+//        bank.getAllClients().get("Kostya").getAccounts().add(s1);
+//        bank.getAllClients().get("Misha").getAccounts().add(c2);
+//        bank.getAllClients().get("Misha").getAccounts().add(s2);
+//        bank.getAllClients().get("Gera").getAccounts().add(c3);
+//        bank.getAllClients().get("Gera").getAccounts().add(s3);
+    }
+    @After
+    public void clear() {
+        bank.getAllClients().clear();
+    }
+
+    @Test
+    public void testFindClientByName() throws ClientNotFoundException {
+        Client kostya = new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro");
+        assertEquals(kostya, clientService.findClientByName(bank, "Kostya"));
+    }
+    @Test (expected = ClientNotFoundException.class)
+    public void testFindClientByNameNotFound() throws ClientNotFoundException {
+        clientService.findClientByName(bank, "Tina");
+    }
+    @Test
+    public void testFindAllClients() {
+        Map<String, Client> clients = new HashMap<>();
+        clients.put("Kostya", new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro"));
+        clients.put("Misha", new Client("Misha", Gender.MALE, "mixa@ukr.net", "+380674567890", "Odessa"));
+        clients.put("Gera",  new Client("Gera", Gender.FEMALE, "gerahello@gmail.com", "+380964561234", "Dnipro"));
+        assertEquals(clients, clientService.findAllClients(bank));
+    }
+    @Test
+    public void testSaveClient() throws ClientAlreadyExistsException {
+        Bank testBank = new Bank();
+        testBank.getAllClients().put("Kostya", new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro"));
+        testBank.getAllClients().put("Misha", new Client("Misha", Gender.MALE, "mixa@ukr.net", "+380674567890", "Odessa"));
+        testBank.getAllClients().put("Gera",  new Client("Gera", Gender.FEMALE, "gerahello@gmail.com", "+380964561234", "Dnipro"));
+        Client testClient = new Client("TestClient", Gender.MALE, "tests@gmail.com", "+380968965212", "TestCity");
+        testBank.getAllClients().put("TestClient", testClient);
+        clientService.saveClient(bank, testClient);
+        assertEquals(testBank.getAllClients(), bank.getAllClients());
+    }
+    @Test (expected = ClientAlreadyExistsException.class)
+    public void testSaveClientAlreadyExists() throws ClientAlreadyExistsException {
+        clientService.saveClient(bank, new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro"));
+    }
+    @Test
+    public void testDeleteClient() throws ClientNotFoundException {
+        Bank testBank = new Bank();
+        testBank.getAllClients().put("Kostya", new Client("Kostya", Gender.MALE, "pro@gmail.com", "+380636908681", "Dnipro"));
+        testBank.getAllClients().put("Misha", new Client("Misha", Gender.MALE, "mixa@ukr.net", "+380674567890", "Odessa"));
+        testBank.getAllClients().put("Gera",  new Client("Gera", Gender.FEMALE, "gerahello@gmail.com", "+380964561234", "Dnipro"));
+        testBank.getAllClients().remove("Gera");
+        clientService.deleteClient(bank, new Client("Gera", Gender.FEMALE, "gerahello@gmail.com", "+380964561234", "Dnipro"));
+        assertEquals(testBank.getAllClients(), bank.getAllClients());
+    }
+    @Test (expected = ClientNotFoundException.class)
+    public void testDeleteClientNotFound() throws ClientNotFoundException {
+        clientService.deleteClient(bank, new Client("Kiril", Gender.MALE, "kirill@mail.com", "+380567893412", "KirilCity"));
+    }
+    @Test
+    public void testAddAccount() {
+        Client vasya = new Client("Vasya", Gender.MALE, "vasya@mail.ru", "+380675674567", "Lviv");
+        Account sAcc = new SavingAccount(500);
+        vasya.getAccounts().add(sAcc);
+        Client petya = new Client("Petya", Gender.MALE, "petya@mail.ru", "+380675674567", "Lviv");
+        clientService.addAccount(petya, sAcc);
+        assertEquals(vasya.getAccounts(), petya.getAccounts());
+    }
+    @Test
+    public void testGetTotalBalance() {
+        Client vasya = new Client("Vasya", Gender.MALE, "vasya@mail.ru", "+380675674567", "Lviv");
+        Account sAcc = new SavingAccount(500);
+        Account chAcc = new CheckingAccount(600, 200);
+        vasya.getAccounts().add(sAcc);
+        vasya.getAccounts().add(chAcc);
+        assertEquals(1100, clientService.getTotalBalance(vasya), 0);
+    }
+    @Test
+    public void testSelectActiveAccount() {
+        Client vasya = new Client("Vasya", Gender.MALE, "vasya@mail.ru", "+380675674567", "Lviv");
+        Account sAcc = new SavingAccount(500);
+        Account chAcc = new CheckingAccount(600, 200);
+        vasya.getAccounts().add(sAcc);
+        vasya.getAccounts().add(chAcc);
+        clientService.selectActiveAccount(vasya, chAcc);
+        assertEquals(chAcc, vasya.getActiveAccount());
+    }
+
+}
