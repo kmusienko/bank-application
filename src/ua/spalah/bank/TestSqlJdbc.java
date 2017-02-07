9110d28b70c6e01fc4db54350cbde20f35e41203
Kostya Musienko <kmusienko97@gmail.com>
2017-02-07 20:03:57 +0200

    Вывод списка клиентов с аккаунтами из базы H2 с помощью JDBC.

--- src/ua/spalah/bank/TestSqlJdbc.java
+++ src/ua/spalah/bank/TestSqlJdbc.java
@@ -44,29 +44,16 @@ public class TestSqlJdbc {
         openConnection();
         try {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM CLIENTS");
-           // ResultSet resAccounts = statement.executeQuery("SELECT * FROM ACCOUNTS");
-
             while (resultSet.next()) {
-                Client client = new Client(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3).equalsIgnoreCase("Male") ? Gender.MALE : Gender.FEMALE, resultSet.getString(5), resultSet.getString(4), resultSet.getString(6));
-//                while(resAccounts.next()) {
-//                    if (resAccounts.getInt(4) == resultSet.getInt(1)) {
-//                        Account account = null;
-//                        switch (resAccounts.getString(3)) {
-//                            case "saving" : account = new SavingAccount(resAccounts.getInt(1));
-//                            case "checking" : account = new CheckingAccount(resAccounts.getInt(1), resAccounts.getInt(2));
-//                        }
-//                        clientService.addAccount(client, account);
-//                    }
- //               }
+                Client client = new Client(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("gender").equalsIgnoreCase("Male") ? Gender.MALE : Gender.FEMALE, resultSet.getString("email"), resultSet.getString("tel"), resultSet.getString("city"));
                 clients.add(client);
             }
             resultSet.close();
-         //   resAccounts.close();
 
             for (Client client: clients) {
                 ResultSet resAccounts = statement.executeQuery("SELECT * FROM ACCOUNTS");
                 while(resAccounts.next()) {
-                    if (resAccounts.getInt(4) == client.getId()) {
+                    if (resAccounts.getInt("client_id") == client.getId()) {
                         Account account = null;
                         switch (resAccounts.getString(3)) {
                             case "saving" : account = new SavingAccount(resAccounts.getInt(1)); break;
